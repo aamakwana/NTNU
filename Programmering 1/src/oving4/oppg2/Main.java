7d7a8e6dbbf9d5d7bae448fa0dcdec9343df1d2e
archimakwana <archiamakwana@gmail.com>
2026-02-13 17:05:31 +0100

    Implement Spiller class with score tracking, dice rolls, and win condition

--- Programmering 1/src/oving4/oppg2/Main.java
+++ Programmering 1/src/oving4/oppg2/Main.java
@@ -1,4 +1,7 @@
 package oving4.oppg2;
 
 public class Main {
+  //have players a and b
+  Spill spillerA = new Spill();
+  Spill spillerB = new Spill();
 }
---
+++ Programmering 1/src/oving4/oppg2/Spill.java
@@ -0,0 +1,40 @@
+package oving4.oppg2;
+import java.util.Random;
+
+public class Spill {
+  //variables
+  Random rand = new Random();
+  private int sumPoeng;
+  private Random terning;
+
+  public Spill() {
+    this.sumPoeng = 0;
+    this.terning = terning;
+
+  }
+
+  public int getSumPoeng() {
+    return sumPoeng;
+  }
+
+  public void kastTerning() {
+    int kast = rand.nextInt(6) + 1; //rand.nextint(6) give 5 therefore add 1
+    if (kast == 1) {
+      sumPoeng = 0;
+    } else {
+      sumPoeng++;
+    }
+
+  }
+
+  public boolean erFerdig() {
+    if (sumPoeng> 100) {
+      return true;
+    } else {
+      return false;
+
+    }
+
+
+  }
+}
