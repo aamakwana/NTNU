695e7d1a1932f2ca8ce04403332992cadcb4e8fb
archimakwana <archiamakwana@gmail.com>
2026-02-18 17:16:20 +0100

    Add WrapTextCommand (oppgave 4 - class 1)

---
+++ Programmering 2/oving_3/src/main/java/no/ntnu/idat/WrapTextCommand.java
@@ -0,0 +1,21 @@
+package no.ntnu.idat;
+
+public class WrapTextCommand implements TextCommand {
+  private final String opening;
+  private final String end;
+  public WrapTextCommand(String opening, String end) {
+    this.opening = opening;
+    this.end = end;
+  }
+  public String getOpening() {
+    return opening;
+  }
+
+  public String getEnd() {
+    return end;
+  }
+  @Override
+  public String execute(String text) {
+    return opening+ text+end;
+  }
+}
