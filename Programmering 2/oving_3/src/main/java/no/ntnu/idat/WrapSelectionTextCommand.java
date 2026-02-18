4de5758a8cf14469ad8c7fe944e0120005cd564c
archimakwana <archiamakwana@gmail.com>
2026-02-18 17:45:27 +0100

    Add WrapSelectionTextCommand (oppgave 4 - class 3)

---
+++ Programmering 2/oving_3/src/main/java/no/ntnu/idat/WrapSelectionTextCommand.java
@@ -0,0 +1,21 @@
+package no.ntnu.idat;
+
+public class WrapSelectionTextCommand extends WrapTextCommand{
+  private String selection;
+
+  public WrapSelectionTextCommand(String opening, String end) {
+    super(opening, end);
+    this.selection = selection;
+
+  }
+  public String getSelection() {
+    return selection;
+
+  }
+  @Override
+  public String execute(String text)
+  {
+    return text.replaceAll(selection, getOpening()+selection+getEnd());
+  }
+
+}
