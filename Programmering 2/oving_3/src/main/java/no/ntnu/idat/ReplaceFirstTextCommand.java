9353b02a4ee455109ed3f1861d56cbc03108fe36
archimakwana <archiamakwana@gmail.com>
2026-02-18 16:45:23 +0100

    Add Replace commands (ReplaceTextCommand, ReplaceFirstTextCommand)  (oppgave 3)

---
+++ Programmering 2/oving_3/src/main/java/no/ntnu/idat/ReplaceFirstTextCommand.java
@@ -0,0 +1,11 @@
+package no.ntnu.idat;
+
+public class ReplaceFirstTextCommand extends ReplaceTextCommand {
+  public ReplaceFirstTextCommand(String target, String replacement) {
+    super(target, replacement);
+  }
+  @Override
+  public String execute(String text) {
+    return text.replaceFirst(getTarget(), getReplacement());
+  }
+}
---
+++ Programmering 2/oving_3/src/main/java/no/ntnu/idat/ReplaceTextCommand.java
@@ -0,0 +1,23 @@
+package no.ntnu.idat;
+
+public class ReplaceTextCommand implements TextCommand {
+  private String target;
+  private String replacement;
+
+  public ReplaceTextCommand(String target, String replacement) {
+    this.target = target;
+    this.replacement = replacement;
+  }
+  public String getTarget() {
+    return target;
+  }
+  public String getReplacement() {
+    return replacement;
+  }
+  @Override
+  public String execute(String text) {
+    return text.replace(target, replacement);
+
+  }
+
+}
