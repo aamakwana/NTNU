62d36f6bfc25748cc6680068ee1030204f18f827
archimakwana <archiamakwana@gmail.com>
2026-02-18 18:17:51 +0100

    Add CapitalizeWordsTextCommand (oppgave 5 - class 2)

---
+++ Programmering 2/oving_3/src/main/java/no/ntnu/idat/CapitalizeWordsTextCommand.java
@@ -0,0 +1,23 @@
+package no.ntnu.idat;
+
+public class CapitalizeWordsTextCommand extends CapitalizeTextCommand {
+  @Override
+  public String execute(String text) {
+    String[] words = text.split(" ");
+    StringBuilder result = new StringBuilder();
+    for (int i = 0; i < words.length; i++) {
+      result.append(words[i].substring(0, 1).toUpperCase() + words[i].substring(1));
+      if (i + 1 < words.length - 1) {
+        result.append(" ");
+      }
+
+    }
+    return result.toString();
+  }
+
+
+
+
+
+
+}
