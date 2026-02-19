1464d14f0d9bfee0cce7b279c7929f22f87436fd
archimakwana <archiamakwana@gmail.com>
2026-02-19 18:27:07 +0100

    Add CapitalizeTextCommandTest

---
+++ Programmering 2/oving_3/src/test/java/no/ntnu/idat/CapitalizeTextCommandTest.java
@@ -0,0 +1,26 @@
+package no.ntnu.idat;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class CapitalizeTextCommandTest {
+  @Test
+  void testCapitalize() {
+    CapitalizeTextCommand result = new CapitalizeTextCommand();
+    assertEquals("Text to be capitalized",
+        result.execute("text to be capitalized"));
+  }
+  @Test
+  void testCapitalizeWords() {
+    CapitalizeTextCommand result = new CapitalizeTextCommand();
+    assertEquals("Text To Be Capitalized",
+        result.execute("text to be capitalized"));
+  }
+  @Test
+  void testCapitalizeSelection() {
+    CapitalizeSelectionTextCommand result = new CapitalizeSelectionTextCommand("capitalized");
+    assertEquals("text to be Capitalized",
+        result.execute("text to be capitalized"));
+  }
+
+}
