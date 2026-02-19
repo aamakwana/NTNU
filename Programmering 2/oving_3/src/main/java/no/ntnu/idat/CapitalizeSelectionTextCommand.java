5c89d84f8f1b791bbf35105651094c6ca3ef787b
archimakwana <archiamakwana@gmail.com>
2026-02-19 18:19:27 +0100

    Fixed the CapitalizeSelectionTextCommand

--- Programmering 2/oving_3/src/main/java/no/ntnu/idat/CapitalizeSelectionTextCommand.java
+++ Programmering 2/oving_3/src/main/java/no/ntnu/idat/CapitalizeSelectionTextCommand.java
@@ -1,17 +1,23 @@
 package no.ntnu.idat;
 
-public class CapitalizeSelectionTextCommand extends CapitalizeTextCommand{
+public class CapitalizeSelectionTextCommand extends CapitalizeTextCommand {
   private String selection;
-  public CapitalizeSelectionTextCommand( String selection) {
+
+  public CapitalizeSelectionTextCommand(String selection) {
     this.selection = selection;
   }
+
   public String getSelection() {
     return selection;
   }
+
   @Override
-  public String execute(String text)
-  {
-    String capitalized = selection.substring(0,1).toUpperCase() + selection.substring(1);
-    return text.replace(selection,capitalized);
+  public String execute(String text) {
+    String capitalized = selection.substring(0, 1).toUpperCase() + selection.substring(1);
+    System.out.println("Looking for: " + selection);
+    System.out.println("Replacing with: " + capitalized);
+    System.out.println("In text: " + text);
+    return text.replace(selection, capitalized);
   }
 }
+
