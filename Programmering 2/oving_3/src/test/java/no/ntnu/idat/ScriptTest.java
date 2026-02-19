1551c524a09a46a99f7623b5fcae43df8e70e6f6
archimakwana <archiamakwana@gmail.com>
2026-02-19 18:32:22 +0100

    Add ScriptTest

---
+++ Programmering 2/oving_3/src/test/java/no/ntnu/idat/ScriptTest.java
@@ -0,0 +1,17 @@
+package no.ntnu.idat;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+class ScriptTest {
+  @Test
+   void testScript() {
+    Script script = new Script(List.of(
+        new ReplaceTextCommand("world", "java"),
+        new CapitalizeTextCommand(),
+        new WrapTextCommand("<p>", "</p>")
+    ));
+    assertEquals("<p>Hello java</p>",
+        script.execute("hello world"));
+  }
+}
